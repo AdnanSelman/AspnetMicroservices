e873203a514cfcda8d5e0c34f0e2c38321bdbf26
Adnan Selman <adnanselman@outlook.com>
2022-08-23 20:35:00 +0200

    fix typo

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -28,7 +28,7 @@ namespace Basket.API
         {
             services.AddStackExchangeRedisCache(options =>
             {
-                options.Configuration = Configuration.GetValue<string>("CacheString:ConnestionString");
+                options.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString");
             });
 
             services.AddControllers();
